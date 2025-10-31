9e549bc379ba7d7433140f3e69990682f5d613b3
Mohammad Bin Ali Soleja <31225174+MohammadSoleja@users.noreply.github.com>
2025-10-31 18:21:47 +0000

    Transaction table personalised, adding transactions now happens in transaction page, upload fixed. (Closes #38 )

--- app_web/models.py
+++ app_web/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class UserTableSetting(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='table_setting')
+    columns = models.JSONField(default=list)  # will be initialized in code if empty
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"TableSetting({self.user})"
